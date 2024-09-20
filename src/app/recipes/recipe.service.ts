d934adc09003081384b8bf7bc0254cf9758f540f
abanoub magdi <abanoubmagdi.4040@gmail.com>
2024-09-21 00:19:06 +0300

    Service Injection Part

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Healthy Meal',
+      'A super-tasty - just awesome!',
+      'https://takethemameal.com/files_images_v2/splash_stam_3.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Ckecken slides',
+      'What else you need to say?',
+      'https://insanelygoodrecipes.com/wp-content/uploads/2021/04/Homemade-Chicken-Tabbouleh-in-a-Bowl-800x530.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
